d02484f5e2aa516afe5e8f4c55d17d8cdcf3ba9c
Dmitry Zhirnov <dzhirnov@atsaero.ru>
2021-11-06 23:24:03 +0300

    Roman To Integer Быстрый метод. Подсмотрел на сайте.

--- roman_to_integer/roman_to_integer.go
+++ roman_to_integer/roman_to_integer.go
@@ -1,29 +1,22 @@
 package roman_to_integer
 
-import (
-	"strings"
-)
-
 func romanToInt(s string) int {
-	romanValues := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
-	romanDigits := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
-	return sum(s, 0, romanDigits, romanValues)
-
-}
-
-func sum(s string, val int, romanDigits []string, romanValues []int) int {
+	romanDigits := map[byte]int{'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
 
 	if len(s) == 0 {
 		return 0
 	}
-	result := 0
-	for index, romanDigit := range romanDigits {
-		position := strings.Index(s, romanDigit)
-		if position == 0 {
-			s = s[len(romanDigit):]
-			result = romanValues[index] + sum(s, val, romanDigits, romanValues)
-			return result
+	result, current, previous := 0, 0, 0
+	for index := len(s) - 1; index >= 0; index-- {
+		current = romanDigits[s[index]]
+		if current >= previous{
+			result += current
 		}
+		if current < previous{
+			result-=current
+		}
+		previous = current
 	}
 	return result
+
 }
